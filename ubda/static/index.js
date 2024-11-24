98bb8922e8a122df3f469fc8a78a23fe9036de70
gious86 <117433187+gious86@users.noreply.github.com>
2024-11-24 14:16:00 +0400

    remove unnecessary files

--- ubda/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteNote(visitorId) {
-  fetch("/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ visitorId: visitorId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
